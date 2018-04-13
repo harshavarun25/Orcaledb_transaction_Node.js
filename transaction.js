4140e3043f006d06a8466fa3c590f31130017cf2
Harsha Varun <harshavarun25@gmail.com>
2018-04-13 11:22:15 -0500

    Update transaction.js

--- transaction.js
+++ transaction.js
@@ -9,13 +9,7 @@ exports.shipmentOrderNo = function(clientRequest, carrierShipResponse) {
 //console.log(clientRequest);
 //console.log(carrierShipResponse);
 
-var sql = "INSERT into Ship_Detail(Ship_No, Pckage_Id, Track_No, Service_Code, User_Id, Item_No, Qty, Length, Width, height, Pckage_Wt, Ship_Date_Entered, Ship_Item_Amount, carrier_item_amount, To_Loc, Decl_Value, "+
-"Sat_Del_Flag, Addl_Handling_Flag, Oversize_Flag, Del_Conf_Flag, Deliver_By, Project_Id, CostCode, Dept_Id, Void_Flag, Ship_Date_Created, Sign_req_flag, Hold_At_Loc_Flag, Res_Delv_Flag, Currency_Type, "+
-"ShipApp_Used, Pending, Closeout_status_id,prod_title, prod_detail_no, NOTIFY_SHPR_FLAG, NOTIFY_RCPT_FLAG, pickup_flag, CARRIER_FRIEGHT_CHARGE, PUBLIC_FRIEGHT_CHARGE, PUBLIC_RATE, BASE_FRIEGHT_CHARGE, "+
-"FUEL_SURCHARGE, DECLVALUE_SURCHARGE, SIGNREQ_CHARGE, SATDELV_CHARGE, SATPICKUP_CHARGE, RESDELV_CHARGE, ADDLHANDL_CHARGE, PICKUP_CHARGE, MISC_CHARGE, TRANSACTION_CHARGE, DEL_CONF_EMAILS, sign_release, "+
-"operating_unit, fund_code, program_code, class_fld, dryice_flag, dryice_wgt, dgoods_flag, dgoods_chrg, ship_type, ship_pay_type, from_loc, shmt_reference, univ_adv_id, out_delv_flag, outdelv_charge, "+
-"consignee_location_name, consignee_contact_name, consignee_address1, consignee_address2, consignee_city, consignee_state_code, consignee_zipcode, consignee_country, consignee_country_name, consignee_phone, "+
-"consignee_email, shipper_location_name, shipper_contact_name, shipper_address1, shipper_address2, shipper_city, shipper_state_code, shipper_zipcode, shipper_country, shipper_country_name, shipper_phone, "+
+var sql = "INSERT into Ship_Detail(shipper_city, shipper_state_code, shipper_zipcode, shipper_country, shipper_country_name, shipper_phone, "+
 "shipper_email, Sunapsis_Internal_Id, Sunapsis_Context_Id, Sunapsis_University_Id, Sunapsis_Department_Id ) "+
 "VALUES ( )"
 
